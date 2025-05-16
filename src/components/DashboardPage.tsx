334536f21a9d94b919299e2dad69abb529f4840f
Sen <115788592+ichishihoness@users.noreply.github.com>
2025-05-16 14:31:47 +0200

    update dashboard page 1

--- src/components/DashboardPage.tsx
+++ src/components/DashboardPage.tsx
@@ -3,9 +3,24 @@ import '../styling/DashboardPage.css';
 
 const DashboardPage: React.FC = () => {
   return (
-    <div>
-      <h1>Dashboard</h1>
-      {/* Voeg hier je dashboard-inhoud toe */}
+    <div className="dashboard-wrapper">
+      <div className="dashboard-sidebar">
+        <hr className="sidebar-divider-2" />
+        <button className="sidebar-btn">Dashboard</button>
+        <button className="sidebar-btn">Kalender</button>
+        <button className="sidebar-btn">Documenten</button>
+        <button className="sidebar-btn">Patiëntenoverzicht</button>
+        <hr className="sidebar-divider-1" />
+        <button className="sidebar-btn">Afspraak toevoegen</button>
+        <button className="sidebar-btn">Sessie toevoegen arts</button>
+        <button className="sidebar-btn">Sessie toevoegen fysiotherapeut</button>
+        <hr className="sidebar-divider-2" />
+        <button className="sidebar-btn">Instellingen</button>
+        <button className="sidebar-btn">Uitloggen</button>
+      </div>
+      <div className="kalender volledige">Kalender volledige</div>
+      <div className="kalender patiënten">Kalender patiënten</div>
+      <div className="notities">Notities</div>
     </div>
   );
 };
